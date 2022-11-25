0118bf79eaad1257a7fdd51f6abdcfeb8d7d5948
Sriram Sankaranarayanan <srirams@iima.ac.in>
2022-11-25 07:30:25 +0530

    Add charFun used in Benedek et al.

--- charFun1.py
+++ charFun1.py
@@ -18,6 +18,26 @@ def charFun(players=None, infoObj = None) -> float:
     ans = sum([v[pp] for pp in players]) - min([w[pp] for pp in players])
     return ans
 
+def charFunBenedek(S, infoObj=None):
+    """
+    Example shown in Finding and verifying the nucleolus of cooperative games - supplement
+    Benedek et al. Math Programming (2021) 190:135-170
+    DOI: 10.1007/s10107-020-01527-9
+    The nucleolus is(2.75, 3.75, 5.5)
+    """
+    l = sorted(list(S))
+    c = {
+        (1,):1,
+        (2,):2,
+        (3,):5,
+        (1,2):6,
+        (1,3):7,
+        (2,3):8,
+        (1,2,3):12
+    }
+    return c[tuple(l)]
+
+
 if __name__ == "__main__":
     import itertools
     n=6
